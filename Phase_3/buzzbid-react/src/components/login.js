bd0ff66e3dc31c020bcbc4523eb18eed6add1c24
Jorge <jnaranjo9@gatech.edu>
2024-04-07 15:21:59 -0700

    Add logo for dashboard and login pages Update dashboard and login page styling

--- Phase_3/buzzbid-react/src/components/login.js
+++ Phase_3/buzzbid-react/src/components/login.js
@@ -1,7 +1,8 @@
 import React, {useRef, useState} from 'react';
 import axios from 'axios';
 import {useNavigate} from 'react-router-dom';
-import {MDBContainer, MDBInput, MDBBtn} from 'mdb-react-ui-kit';
+import {MDBContainer, MDBInput, MDBBtn, MDBCol, MDBRow} from 'mdb-react-ui-kit';
+import buzzLogo from '../images/buzz.png';
 
 function Login() {
     const[username, setUsername] = useState('');
@@ -33,21 +34,37 @@ function Login() {
 
     return (
         <div className="d-flex justify-content-center align-items-center vh-100">
-            <div className="border rounded-lg p-4" style={{width: '500px', height: 'auto'}}>
+            <div className="border rounded-lg p-4" style={{width: 'auto', height: 'auto'}}>
                 <MDBContainer className="p-3">
-                    <h1 className="mb-4 text-center">Buzzbid</h1>
-                    <h3 className="mb-4 text-center">It's not junk if someone will pay for it.</h3>
-                    <MDBInput wrapperClass='mb-4' placeholder='Username' id='username' value={username} type='email'
-                              onChange={(e) => setUsername(e.target.value)}/>
-                    <MDBInput wrapperClass='mb-4' placeholder='Password' id='password' type='password' value={password}
-                              onChange={(e) => setPassword(e.target.value)}/>
-                    {error && <p className="text-danger">{error}</p>}
-                    <button className="mb-4 d-block btn-primary" style={{height: '50px', width: '100%'}}
-                            onClick={handleLogin}>Log In
-                    </button>
-                    <div className="text-center">
-                        <p>Not registered? <a href="/register">Register</a></p>
-                    </div>
+                    <MDBRow>
+                        <MDBCol md="6">
+                            <img width={250} height={250} src={buzzLogo} alt="buzz" />
+                        </MDBCol>
+                        <MDBCol md="6">
+                            <h1 className="mb-4 text-center">Buzzbid</h1>
+                            <h3 className="mb-4 text-center">It's not junk if someone will pay for it.</h3>
+                        </MDBCol>
+                    </MDBRow>
+                    <br/>
+                    <MDBRow>
+                        <MDBCol md="12">
+                            <MDBInput wrapperClass='mb-4' placeholder='Username' id='username' value={username} type='email'
+                                      onChange={(e) => setUsername(e.target.value)}/>
+                            <MDBInput wrapperClass='mb-4' placeholder='Password' id='password' type='password' value={password}
+                                      onChange={(e) => setPassword(e.target.value)}/>
+                            {error && <p className="text-danger">{error}</p>}
+                        </MDBCol>
+                    </MDBRow>
+                    <MDBRow>
+                        <MDBCol md="12">
+                            <button className="mb-4 d-block btn-primary" style={{height: '50px', width: '100%'}}
+                                    onClick={handleLogin}>Log In
+                            </button>
+                            <div className="text-center">
+                                <p>Not registered? <a href="/register">Register</a></p>
+                            </div>
+                        </MDBCol>
+                    </MDBRow>
                 </MDBContainer>
             </div>
         </div>
