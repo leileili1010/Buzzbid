70b402746048d4def53ad2c92f62bfcf58903763
Li, Lei <lli681@gatech.edu>
2024-04-11 16:31:21 -0700

    modal

--- Phase_3/buzzbid-react/src/components/ItemRating/ItemRating.jsx
+++ Phase_3/buzzbid-react/src/components/ItemRating/ItemRating.jsx
@@ -1,20 +1,23 @@
 import {NavLink} from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import {useParams, useLocation} from "react-router-dom";
+import {useParams, useNavigate} from "react-router-dom";
 import {thunkGetRatings} from "../../redux/rating";
 import {thunkGetItemWithAvgRating} from "../../redux/item";
 import "./ItemRating.css";
 import {generateStars, formatDate} from "../helperFunctions/helperFunctions";
+import OpenModalButton from "../OpenModalButton";
+import DeleteRatingModal from "./DeleteRatingModal";
 
 const ItemRating = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const {itemId} = useParams();
     const item = useSelector(state => state.item[itemId])
     const ratingsObj = useSelector(state => state.rating)
     const ratings = Object.values(ratingsObj);
-    const jsonString = localStorage.getItem('user');
-    const currentUser = JSON.parse(jsonString);
+    const userJsonString = localStorage.getItem('user');
+    const currentUser = JSON.parse(userJsonString);
 
     let averageRating;
     if (parseInt(item?.avgRating) > 1) {
@@ -24,6 +27,10 @@ const ItemRating = () => {
     } else {
         averageRating = item?.avgRating;
     }
+
+    useEffect(() => {
+        if (!userJsonString) navigate("/login");
+    }, [userJsonString, navigate]);
 
     useEffect(() => {
         dispatch(thunkGetRatings(itemId));
@@ -40,33 +47,41 @@ const ItemRating = () => {
             <table className="item-rating-item">
                 <tbody>
                 <tr>
-                    <td>Item ID</td>
+                    <td className="table-title">Item ID</td>
                     <td>{item?.itemId}</td>
                 </tr>
                 <tr>
-                    <td>Item Name</td>
+                    <td className="table-title">Item Name</td>
                     <td>{item?.itemName}</td>
                 </tr>
                 <tr>
-                    <td>Average Rating</td>
+                    <td className="table-title">Average Rating</td>
                     <td>{averageRating}</td>
                 </tr>
                 </tbody>
             </table>
             <div className="comment-rating-container">
                 {ratings.map((rating, index) =>
-                    <div key={index}>
-                        <div>
-                            <div><i className="fa-solid fa-user" id="user-icon"></i></div>
-                            <p>{rating?.username}</p>
+                    <div key={index} className="ratings">
+                        <div className="user-star">
+                            <div className="user-details">
+                                <div><i className="fa-solid fa-user" id="user-icon"></i></div>
+                                <p>{rating?.username}</p>
+                            </div>
+                            <div className="stars">
+                                {generateStars(rating)}
+                            </div>
                         </div>
-                        {generateStars(rating)}
-                        <p>{formatDate(rating?.ratingTime)}</p>
-                        <p>{rating?.comment}</p>
-                        {rating?.username == currentUser.username &&  <button>Delete</button>}
+                        <p className="rating-time">{formatDate(rating?.ratingTime)}</p>
+                        <p className="rating-comment">{rating?.comment}</p>
+                        {rating?.username == currentUser.username &&
+                            < OpenModalButton
+                                buttonText="Delete"
+                                modalComponent={<DeleteRatingModal ratingId={rating?.ratingId} />}
+                            />
+                        }
                     </div>
                 )}
-
             </div>
         </div>
     )
